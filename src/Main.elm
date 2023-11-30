5b551aa5bde07417c200338d038d06ffd41001f2
Anja Hildebrandt <anja.hildebrandt@gmx.de>
2023-11-30 22:07:18 +0100

    Start setting up pages

--- src/Main.elm
+++ src/Main.elm
@@ -1,28 +1,43 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Browser.Navigation
+import Html exposing (Html, div, h1, h2, img, text)
 import Html.Attributes exposing (src)
+import Url exposing (Url)
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { key : Browser.Navigation.Key
+    , page : Page
+    }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+init : Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
+init url key =
+    ( { page = Overview
+      , key = key
+      }
+    , Cmd.none
+    )
 
 
 
 ---- UPDATE ----
 
 
+type Page
+    = Overview
+    | Day01
+
+
 type Msg
-    = NoOp
+    = ClickedLink Browser.UrlRequest
+    | ChangedUrl Url
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -39,6 +54,7 @@ view model =
     div []
         [ img [ src "/logo.svg" ] []
         , h1 [] [ text "Your Elm App is working!" ]
+        , h2 [] [ text "Really!" ]
         ]
 
 
@@ -48,9 +64,11 @@ view model =
 
 main : Program () Model Msg
 main =
-    Browser.element
-        { view = view
+    Browser.application
+        { view = \model -> { title = "aoc2023", body = [ view model ] }
         , init = \_ -> init
         , update = update
         , subscriptions = always Sub.none
+        , onUrlChange = ChangedUrl
+        , onUrlRequest = ClickedLink
         }
