f798051894e9325e3e914e97ebab0ac8e0f06fe6
Anja Hildebrandt <anja.hildebrandt@gmx.de>
2023-11-30 23:19:24 +0100

    Start setting up pages

--- src/Pages/Overview.elm
+++ src/Pages/Overview.elm
@@ -1,7 +1,7 @@
 module Pages.Overview exposing (..)
 
-import Html exposing (Html, div, h1, h2, img, text)
-import Html.Attributes exposing (src)
+import Html exposing (Html, a, div, h1, h2, img, text)
+import Html.Attributes exposing (href, src)
 
 
 init : Model
@@ -15,6 +15,7 @@ view _ =
         [ img [ src "/logo.svg" ] []
         , h1 [] [ text "Your Elm App is working!" ]
         , h2 [] [ text "Really!" ]
+        , a [ href "#/day01" ] [ text "Day01" ]
         ]
 
 
