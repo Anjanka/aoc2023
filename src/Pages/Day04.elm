c0a4abd8a3a2d160928dc0a27bfaef0c520b90b4
Anja Hildebrandt <anja.hildebrandt@gmx.de>
2023-12-20 11:53:12 +0100

    Add solution for Day04 Part1

--- src/Pages/Day04.elm
+++ src/Pages/Day04.elm
@@ -3,6 +3,8 @@ module Pages.Day04 exposing (..)
 import Html exposing (Html, button, div, h1, p, text, textarea)
 import Html.Attributes exposing (placeholder, value)
 import Html.Events exposing (onClick, onInput)
+import List.Extra
+import Set exposing (Set)
 
 
 init : Model
@@ -48,4 +50,64 @@ update msg model =
             { model | content = newContent, result = "", result2 = "" }
 
         GetWinningPoints ->
-            { model | result = "" }
+            { model | result = String.fromInt (resultPart1 model.content) }
+
+
+listsOfNumbers : String -> List (List (List Int))
+listsOfNumbers scratchcards =
+    scratchcards
+        |> String.lines
+        |> List.map
+            (\s ->
+                String.split ":" s
+                    |> List.Extra.last
+                    |> Maybe.withDefault ""
+                    |> String.split "|"
+                    |> List.map
+                        (\n ->
+                            String.words n
+                                |> List.map
+                                    (\w ->
+                                        String.toInt w
+                                            |> Maybe.withDefault 0
+                                    )
+                        )
+            )
+
+
+winningNumbers : List (List Int) -> Int
+winningNumbers listOfNums =
+    listOfNums
+        |> List.map Set.fromList
+        |> section
+
+
+section : List (Set Int) -> Int
+section card =
+    card
+        |> (\s -> Set.intersect (Maybe.withDefault Set.empty (List.head s)) (Maybe.withDefault Set.empty (List.Extra.last s)))
+        |> Set.size
+
+
+points : List Int -> Int
+points winNum =
+    winNum
+        |> List.map (\n -> sectionToPoints n)
+        |> List.sum
+
+
+sectionToPoints : Int -> Int
+sectionToPoints n =
+    if n <= 0 then
+        0
+
+    else
+        2 ^ (n - 1)
+
+
+resultPart1 : String -> Int
+resultPart1 scratchcards =
+    scratchcards
+        |> listsOfNumbers
+        |> List.map winningNumbers
+        |> points
